24fcd9271f67024e3121dc13740f0f5032319e97
Matias <mgaray@genexusconsulting.com>
2026-03-09 20:22:21 -0300

    1 commit frontedn

--- backend/src/main/java/com/despachantes/api/ApiApplication.java
+++ backend/src/main/java/com/despachantes/api/ApiApplication.java
@@ -9,5 +9,5 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	// Comentario de prueba para validar el flujo de Git
 }
